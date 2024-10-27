084bcc8256736970d5b3f58f6bbf84d128c2f18d
i-teegee <israelthankgod@gmail.com>
2024-10-27 04:26:20 +0100

    babygoat first commit

--- functions.py
+++ functions.py
@@ -32,13 +32,13 @@ API_HASH = os.getenv('API_HASH')
 # STICKER_NAME = os.getenv('STICKER_NAME')
 
 
-STICKER_NAME = 'captaindolan'
+STICKER_NAME = 'cryptonotes'
 
-project_group_link = 'https://t.me/+Ii5ka-MK7pU3NjAx'
+project_group_link = 'https://t.me/+eku73dgS2e9mMjE5'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'Ii5ka-MK7pU3NjAx'
+project_short_name = 'eku73dgS2e9mMjE5'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
