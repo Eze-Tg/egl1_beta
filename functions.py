1fa4f560dda408f899099af99a0711158ef6559b
i-teegee <israelthankgod@gmail.com>
2024-11-22 08:32:06 +0100

    FC kacy

--- functions.py
+++ functions.py
@@ -32,13 +32,13 @@ API_HASH = os.getenv('API_HASH')
 # STICKER_NAME = os.getenv('STICKER_NAME')
 
 
-STICKER_NAME = 'chillguystickers'
+STICKER_NAME = 'Kacy1'
 
-project_group_link = 'https://t.me/+BgShX7zn95BmYzhl'
+project_group_link = 'https://t.me/+QF0G_qZx1ggwMTJl'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'BgShX7zn95BmYzhl'
+project_short_name = 'QF0G_qZx1ggwMTJl'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
