433d1aaf35c1ba367d3aa102bb81a1af225d2dba
i-teegee <israelthankgod@gmail.com>
2024-11-23 11:26:41 +0100

    FC luce

--- functions.py
+++ functions.py
@@ -32,13 +32,13 @@ API_HASH = os.getenv('API_HASH')
 # STICKER_NAME = os.getenv('STICKER_NAME')
 
 
-STICKER_NAME = 'dolosthebully'
+STICKER_NAME = 'believeinluce_by_fStikBot'
 
-project_group_link = 'https://t.me/+BUTZTDWgrWU5MzQ1'
+project_group_link = 'https://t.me/+s2lbDVdjg3E3ZjNl'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'BUTZTDWgrWU5MzQ1'
+project_short_name = 's2lbDVdjg3E3ZjNl'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
