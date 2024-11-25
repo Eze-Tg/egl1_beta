ab731fb8a567b1603d7c42f4d369d4778af4d774
i-teegee <israelthankgod@gmail.com>
2024-11-25 10:04:34 +0100

    FC - Purple_beta

--- functions.py
+++ functions.py
@@ -32,13 +32,13 @@ API_HASH = os.getenv('API_HASH')
 # STICKER_NAME = os.getenv('STICKER_NAME')
 
 
-STICKER_NAME = 'Crocosaurus'
+STICKER_NAME = 'GameEmoji'
 
-project_group_link = 'https://t.me/+BQSgYzbiLKQ1NzVl'
+project_group_link = 'https://t.me/+d36cTJNG7YEzNTM1'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'BQSgYzbiLKQ1NzVl'
+project_short_name = 'd36cTJNG7YEzNTM1'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
