0ccb904212305d43819ed14e94ff064cce6ba29f
i-teegee <israelthankgod@gmail.com>
2024-11-30 17:13:58 +0100

    Morty fC

--- functions.py
+++ functions.py
@@ -32,13 +32,13 @@ API_HASH = os.getenv('API_HASH')
 # STICKER_NAME = os.getenv('STICKER_NAME')
 
 
-STICKER_NAME = 'wenmoonpack'
+STICKER_NAME = 'Jessica690'
 
-project_group_link = 'https://t.me/+d36cTJNG7YEzNTM1'
+project_group_link = 'https://t.me/+8h6-TsUwxTwxOWJl'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'd36cTJNG7YEzNTM1'
+project_short_name = '8h6-TsUwxTwxOWJl'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
