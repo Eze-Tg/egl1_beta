a1de561168892c5480231d25d7d3f7869361e200
i-teegee <israelthankgod@gmail.com>
2024-12-01 18:01:01 +0100

    new group

--- functions.py
+++ functions.py
@@ -34,11 +34,11 @@ API_HASH = os.getenv('API_HASH')
 
 STICKER_NAME = 'USDSITCOM_by_fStikBot'
 
-project_group_link = 'https://t.me/+5m23QN-26OAzMTJl'
+project_group_link = 'https://t.me/+3QlS7SBXSmM2ZjI1'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = '5m23QN-26OAzMTJl'
+project_short_name = '3QlS7SBXSmM2ZjI1'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
