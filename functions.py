dfbf21dd3e0ce2a778edf594a66054f9228f06d5
i-teegee <israelthankgod@gmail.com>
2024-12-02 17:29:11 +0100

    First COmmit

--- functions.py
+++ functions.py
@@ -32,13 +32,13 @@ API_HASH = os.getenv('API_HASH')
 # STICKER_NAME = os.getenv('STICKER_NAME')
 
 
-STICKER_NAME = 'CryptoNotes'
+STICKER_NAME = 'CHEXFOLK'
 
-project_group_link = 'https://t.me/+bkYxs4mGb-tlNjU1'
+project_group_link = 'https://t.me/+553Ar4h5jRE0Mjc1'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'bkYxs4mGb-tlNjU1'
+project_short_name = '553Ar4h5jRE0Mjc1'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
