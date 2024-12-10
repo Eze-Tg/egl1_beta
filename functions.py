f41f79a609015bc6412018da357c1fee22371db8
i-teegee <israelthankgod@gmail.com>
2024-12-10 09:45:47 +0100

    first commit

--- functions.py
+++ functions.py
@@ -32,13 +32,13 @@ API_HASH = os.getenv('API_HASH')
 # STICKER_NAME = os.getenv('STICKER_NAME')
 
 
-STICKER_NAME = 'Honey2803'
+STICKER_NAME = 'Biaotokensol'
 
-project_group_link = 'https://t.me/+mWHh2tcUg4IyMmM1'
+project_group_link = 'https://t.me/+wnJKbd1ps6Y3Zjg9'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'mWHh2tcUg4IyMmM1'
+project_short_name = 'wnJKbd1ps6Y3Zjg9'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
