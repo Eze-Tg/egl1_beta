fb8b5986eb9e7daf64a6417c03d437d2971ae433
i-teegee <israelthankgod@gmail.com>
2024-12-23 14:52:16 +0100

    new profiles

--- functions.py
+++ functions.py
@@ -34,11 +34,11 @@ API_HASH = os.getenv('API_HASH')
 
 STICKER_NAME = 'Biaotokensol'
 
-project_group_link = 'https://t.me/+BtoFtP0QYzpkNDE1'
+project_group_link = 'https://t.me/+JWb2tJtmJktjNDM8'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'BtoFtP0QYzpkNDE1'
+project_short_name = 'JWb2tJtmJktjNDM8'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
