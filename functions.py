fab3493a50d06813bc9b450cb6d075cae5ff9697
i-teegee <israelthankgod@gmail.com>
2024-12-27 10:48:23 +0100

    new profiles added

--- functions.py
+++ functions.py
@@ -34,11 +34,11 @@ API_HASH = os.getenv('API_HASH')
 
 STICKER_NAME = 'AskAnyGPT'
 
-project_group_link = 'https://t.me/+TDK8t5w0ZJ81OTY1'
+project_group_link = 'https://t.me/+14qqNwjUOJoxOGM9'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'TDK8t5w0ZJ81OTY1'
+project_short_name = '14qqNwjUOJoxOGM9'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
