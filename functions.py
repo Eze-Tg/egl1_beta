c61f19851e990d952b90561d867e61270e5d35dc
i-teegee <israelthankgod@gmail.com>
2024-12-30 00:17:25 +0100

    fc

--- functions.py
+++ functions.py
@@ -32,13 +32,13 @@ API_HASH = os.getenv('API_HASH')
 # STICKER_NAME = os.getenv('STICKER_NAME')
 
 
-STICKER_NAME = 'TSOTCHKE01'
+STICKER_NAME = 'AskAnyGPT'
 
-project_group_link = 'https://t.me/+H_rZdLeLusgwNmE1'
+project_group_link = 'https://t.me/+S7g2_phRARExMjU1'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'H_rZdLeLusgwNmE1'
+project_short_name = 'S7g2_phRARExMjU1'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
