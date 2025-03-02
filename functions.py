620410c04ca4531821017c261845ac704f23a9e1
Eze-Tg <israelthankgod@gmail.com>
2025-03-02 07:31:39 +0100

    fc

--- functions.py
+++ functions.py
@@ -34,11 +34,11 @@ API_HASH = os.getenv('API_HASH')
 
 STICKER_NAME = 'AskAnyGPT'
 
-project_group_link = 'https://t.me/+ht5uwL3iZNgxM2M9'
+project_group_link = 'https://t.me/+Z18IQlR9lnQ0NzY1'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = 'ht5uwL3iZNgxM2M9'
+project_short_name = 'Z18IQlR9lnQ0NzY1'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
