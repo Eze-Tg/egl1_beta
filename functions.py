ce916895bec9aae4bbec23fdc334097eb9b200d2
Eze-Tg <israelthankgod@gmail.com>
2025-06-03 11:26:31 +0100

    fc

--- functions.py
+++ functions.py
@@ -34,10 +34,10 @@ API_HASH = os.getenv('API_HASH')
 
 STICKER_NAME = 'AskAnyGPT'
 
-project_group_link = 'https://t.me/+-vU02OtTPDZkYjIx'
+project_group_link = 'https://t.me/+2yoRdO4QXK84Zjg1'
 # project_group_link = os.getenv('PROJECT_GROUP_LINK')
 # project_short_name = os.getenv('PROJECT_GROUP_LINK_SHORT')
-project_short_name = '-vU02OtTPDZkYjIx'
+project_short_name = '2yoRdO4QXK84Zjg1'
 
 # project_group_id  = os.getenv('GROUP_ID')
 # project_group_id = -1002172471915
